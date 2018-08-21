8f3e0c0edd7892108e5cf26029651affa13c214f
Lirian Su <liriansu@gmail.com>
2018-08-21 15:42:26 +0800

    feat: implement main logic

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pylint_topology',
+    version='0.1.0',
+    description='check module topology in pylint',
+    author='Lirian Su',
+    author_email='liriansu@gmail.com',
+    url='',
+    license='MIT License',
+    packages=['pylint_topology'],
+    install_requirements=['pylint'],
+)
