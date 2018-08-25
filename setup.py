90ea549de1b5f1fead41be78f38cc2d6d8899949
Lirian Su <liriansu@gmail.com>
2018-08-25 22:52:46 +0800

    feat: add setup.cfg

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name='pylint_topology',
+    name='pylint-topology',
     version='0.1.0',
     description='check module topology in pylint',
     author='Lirian Su',
     author_email='liriansu@gmail.com',
-    url='',
-    license='MIT License',
+    url='https://github.com/LKI/pylint_topology',
+    license='GPLv2',
     packages=['pylint_topology'],
-    install_requirements=['pylint'],
+    install_requirements=['pylint>=2.0'],
 )
