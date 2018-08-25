b2488d1505363bf2767d9f94dcfcc63fac2a3343
Lirian Su <liriansu@gmail.com>
2018-08-25 22:54:17 +0800

    fix: install requires

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name='pylint-topology',
-    version='0.1.0',
+    version='0.1.1',
     description='check module topology in pylint',
     author='Lirian Su',
     author_email='liriansu@gmail.com',
     url='https://github.com/LKI/pylint_topology',
     license='GPLv2',
     packages=['pylint_topology'],
-    install_requirements=['pylint>=2.0'],
+    install_requires=['pylint>=2.0'],
 )
